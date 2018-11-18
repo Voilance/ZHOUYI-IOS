4913d554e670f29b8972e2b9279c62deb362e775
Voilance <voilance@qq.com>
2018-11-18 18:03:12 +0800

    画了Register的页面

---
+++ ZHOUYI/Login.swift
@@ -0,0 +1,30 @@
+//
+//  Login.swift
+//  ZHOUYI
+//
+//  Created by ZHUPF on 2018/11/18.
+//  Copyright © 2018年 FENGYX. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
