d5c15e8bb0ba2dbebaa52a150cc001c9927060cc
Voilance <voilance@qq.com>
2018-11-28 18:34:38 +0800

    almost画好了布褂结果的布局

---
+++ ZHOUYI/Result.swift
@@ -0,0 +1,30 @@
+//
+//  Result.swift
+//  ZHOUYI
+//
+//  Created by ZHUPF on 2018/11/28.
+//  Copyright © 2018年 FENGYX. All rights reserved.
+//
+
+import UIKit
+
+class Result: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
